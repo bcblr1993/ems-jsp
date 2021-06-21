d9c184244b0a227fec39c8d06e0a4f9735300141
xiaochen <chenynqc@163.com>
2021-06-21 15:39:33 +0800

    feat: 加入了 RedirectAttributes 对象的使用

--- src/main/java/com/baizhi/emsjsp/controller/EmpController.java
+++ src/main/java/com/baizhi/emsjsp/controller/EmpController.java
@@ -18,56 +18,60 @@ public class EmpController {
 
     /**
      * 员工列表
+     *
      * @param request
      * @return
      */
     @RequestMapping("lists")
-    public String lists(HttpServletRequest request){
+    public String lists(HttpServletRequest request) {
         List<Emp> lists = empService.lists();
-        request.setAttribute("lists",lists);
+        request.setAttribute("lists", lists);
         return "ems/emplist";
     }
 
     /**
      * 删除员工
+     *
      * @param id
      * @return
      */
     @RequestMapping("delete")
-    public String delete(Integer id){
+    public String delete(Integer id) {
         empService.delete(id);
         return "redirect:/lists";
     }
 
     /**
      * 保存员工信息
+     *
      * @param emp
      * @return
      */
     @RequestMapping("save")
-    public String save(Emp emp){
+    public String save(Emp emp) {
         empService.save(emp);
         return "redirect:/lists";
     }
 
     /**
      * 查询一个
+     *
      * @param id
      * @return
      */
 
     @RequestMapping("queryById")
-    public String queryById(Integer id,HttpServletRequest request){
+    public String queryById(Integer id, HttpServletRequest request) {
         Emp emp = empService.queryById(id);
-        request.setAttribute("emp",emp);
-        return  "/ems/updateEmp";
+        request.setAttribute("emp", emp);
+        return "/ems/updateEmp";
     }
 
     /**
      * 更新信息
      */
     @RequestMapping("update")
-    public String update(Emp emp){
+    public String update(Emp emp) {
         empService.update(emp);
         return "redirect:/lists";
     }
--- src/main/java/com/baizhi/emsjsp/controller/UserController.java
+++ src/main/java/com/baizhi/emsjsp/controller/UserController.java
@@ -6,6 +6,7 @@ import com.baizhi.emsjsp.utils.VerifyCodeUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -20,27 +21,29 @@ public class UserController {
     private UserService userService;
 
     @RequestMapping("login")
-    public String login(User user,HttpSession session){
+    public String login(User user, HttpSession session, RedirectAttributes redirectAttributes) {
         User userDB = userService.login(user);
-        if(userDB==null){
+        if (userDB == null) {
+            redirectAttributes.addFlashAttribute("msg", "用户名不存在!");
             return "redirect:/ems/login.jsp";
         }
-        session.setAttribute("user",userDB);
+        session.setAttribute("user", userDB);
         return "redirect:/lists";
     }
 
     /**
      * 用户注册
+     *
      * @param user
      * @param code
      * @param session
      * @return
      */
     @RequestMapping("register")
-    public String register(User user,String code,HttpSession session){
+    public String register(User user, String code, HttpSession session) {
         //1.比较验证码
         String sessionCode = session.getAttribute("code").toString();
-        if(sessionCode.equalsIgnoreCase(code)){
+        if (sessionCode.equalsIgnoreCase(code)) {
             //2.注册用户
             userService.save(user);
             return "redirect:/ems/login.jsp";
@@ -50,6 +53,7 @@ public class UserController {
 
     /**
      * 生成验证码
+     *
      * @param response
      * @param request
      * @throws IOException
@@ -63,6 +67,6 @@ public class UserController {
         session.setAttribute("code", code);
         //3.生成图片
         response.setContentType("image/png");
-        VerifyCodeUtils.outputImage(220,80,response.getOutputStream(),code);
+        VerifyCodeUtils.outputImage(220, 80, response.getOutputStream(), code);
     }
 }
